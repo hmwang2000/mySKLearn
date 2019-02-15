7cb77f1142c9726a972422b610558c674efa41a7
hmwang2000 <hmwang2000@yahoo.com>
2019-02-15 13:59:11 +0800

    TF Study

--- Keras/Zero/HelloWorld.py
+++ Keras/Zero/HelloWorld.py
@@ -1,52 +1,29 @@
-# Regressor example
-# Code: https://github.com/keloli/KerasPractise/edit/master/Regressor.py
-
-import numpy as np
-np.random.seed(1337)
+import keras
 from keras.models import Sequential
-from keras.layers import Dense
-import matplotlib.pyplot as plt
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
 
-# 创建数据集
-X = np.linspace(-1, 1, 200)
-np.random.shuffle(X)    # 将数据集随机化
-Y = 0.5 * X + 2 + np.random.normal(0, 0.05, (200, )) # 假设我们真实模型为：Y=0.5X+2
-# 绘制数据集plt.scatter(X, Y)
-plt.show()
+# 生成虚拟数据
+import numpy as np
 
-X_train, Y_train = X[:160], Y[:160]     # 把前160个数据放到训练集
-X_test, Y_test = X[160:], Y[160:]       # 把后40个点放到测试集
+x_train = np.random.random((1000, 20))
+y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+x_test = np.random.random((100, 20))
+y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
 
-# 定义一个model，
-model = Sequential () # Keras有两种类型的模型，序贯模型（Sequential）和函数式模型
-                      # 比较常用的是Sequential，它是单输入单输出的
-model.add(Dense(output_dim=1, input_dim=1)) # 通过add()方法一层层添加模型
-                                            # Dense是全连接层，第一层需要定义输入，
-                                            # 第二层无需指定输入，一般第二层把第一层的输出作为输入
+model = Sequential()
+# Dense(64) 是一个具有 64 个隐藏神经元的全连接层。
+#  在第一层必须指定所期望的输入数据尺寸：
+#  在这里，是一个 20 维的向量。
+model.add(Dense(64, activation='relu', input_dim=20))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=20, batch_size=128)
 
-# 定义完模型就需要训练了，不过训练之前我们需要指定一些训练参数
-# 通过compile()方法选择损失函数和优化器
-# 这里我们用均方误差作为损失函数，随机梯度下降作为优化方法
-model.compile(loss='mse', optimizer='sgd')
+score = model.evaluate(x_test, y_test, batch_size=128)
 
-# 开始训练
-print('Training -----------')
-for step in range(301):
-    cost = model.train_on_batch(X_train, Y_train) # Keras有很多开始训练的函数，这里用train_on_batch（）
-    if step % 100 == 0:
-        print('train cost: ', cost)
-
-# 测试训练好的模型
-print('\nTesting ------------')
-cost = model.evaluate(X_test, Y_test, batch_size=40)
-print('test cost:', cost)
-W, b = model.layers[0].get_weights()    # 查看训练出的网络参数
-                                        # 由于我们网络只有一层，且每次训练的输入只有一个，输出只有一个
-                                        # 因此第一层训练出Y=WX+B这个模型，其中W,b为训练出的参数
-print('Weights=', W, '\nbiases=', b)
-
-# plotting the prediction
-Y_pred = model.predict(X_test)
-plt.scatter(X_test, Y_test)
-plt.plot(X_test, Y_pred)
-plt.show()
+print(score)
